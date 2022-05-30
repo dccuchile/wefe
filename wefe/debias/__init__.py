a20af865f774b37cc0aecaef4ff7b9b348e38708
María José Zambrano <mzambrano@ug.uchile.cl>
2022-05-29 23:20:07 -0400

    repulsion attraction neutralization added to init

--- wefe/debias/__init__.py
+++ wefe/debias/__init__.py
@@ -1,2 +1,3 @@
 from wefe.debias.hard_debias import HardDebias
 from wefe.debias.multiclass_hard_debias import MulticlassHardDebias
+from wefe.debias.repulsion_attraction_neutralization import RepulsionAttractionNeutralization

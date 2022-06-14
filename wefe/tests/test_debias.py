06c40121cd797fc8200a4291a597289136f4b5dc
María José Zambrano <mzambrano@ug.uchile.cl>
2022-06-13 22:10:51 -0400

    error in test model path fixed

--- wefe/tests/test_debias.py
+++ wefe/tests/test_debias.py
@@ -22,7 +22,7 @@ def model() -> WordEmbeddingModel:
     WordEmbeddingModel
         The loaded testing model.
     """
-    w2v = KeyedVectors.load("wefe/tests/test_debias.py")  
+    w2v = KeyedVectors.load("./wefe/tests/w2v_test.kv")  
     return WordEmbeddingModel(w2v, "word2vec")
 
 

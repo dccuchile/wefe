1f2f2dcc5b7cc49146b500f56ee8b5967a397f6f
Pablo Badilla <pbadilla.torrealba@gmail.com>
2025-07-22 11:35:13 -0400

    feature: update base metric

--- wefe/metrics/RND.py
+++ wefe/metrics/RND.py
@@ -261,7 +261,12 @@ class RND(BaseMetric):
 
         """
         # check the types of the provided arguments (only the defaults).
-        self._check_input(query, model, locals())
+        self._check_input(
+            query=query,
+            model=model,
+            lost_vocabulary_threshold=lost_vocabulary_threshold,
+            warn_not_found_words=warn_not_found_words,
+        )
 
         # transform query word sets into embeddings
         embeddings = get_embeddings_from_query(
